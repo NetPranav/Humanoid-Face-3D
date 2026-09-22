62928168fda5024db4eb0d779a7e81ae818ef6f0
Pranav Dubey <pranav@Mac.mshome.net>
2026-09-22 23:42:22 +0530

    feat(stage3): implement Tier 2 photometric meso wrinkle engine and unit tests

--- src/stage3_detail/__init__.py
+++ src/stage3_detail/__init__.py
@@ -4,6 +4,7 @@ Stage 3 Micro-Detail GAN Module.
 from src.stage3_detail.generator import DetailGenerator
 from src.stage3_detail.discriminator import DetailDiscriminator
 from src.stage3_detail.inference import DetailSynthesizer
+from src.stage3_detail.photometric_detail import PhotometricDetailExtractor
 from src.stage3_detail.rasterizer import (
     load_flame_uv_layout,
     compute_vertex_normals,
@@ -17,6 +18,7 @@ __all__ = [
     'DetailDiscriminator',
     'MultiScalePatchGAN',
     'DetailSynthesizer',
+    'PhotometricDetailExtractor',
     'load_flame_uv_layout',
     'compute_vertex_normals',
     'rasterize_uv_maps',
